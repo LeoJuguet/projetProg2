5b5449366f0be7d2576afbb0f0a9bab2eeb5d63c
Tristan Parcollet <tristan.parcollet@orange.fr>
2023-03-27 14:32:24 +0200

    Modification de ProgressBar

--- src/main/scala/GUI/ProgressBar.scala
+++ src/main/scala/GUI/ProgressBar.scala
@@ -7,14 +7,14 @@ import gui.{VerticalBox,UIComponent}
 import gui.Style
 import scala.math._
 
-//A prendre globalement comme un schema de code et pas vraiment du code pour l'instant
-val ProgressBarFillStyle = Style (
-    fillColor = Color.Green(),
-    outlineThickness = 0f
-)
-
-val bgStyle = ShapeStyle()
-val fgStyle = ShapeStyle(style = ProgressBarFillStyle)
+class ProgressBarStyle(
+    var backGroundStyle: ShapeStyle = ShapeStyle(),
+    var foreGroundStyle: ShapeStyle = ShapeStyle(style = Style(fillColor = Color.Green(),outlineThickness=0f)))
+{
+    def apply(backGround: Shape,foreGround: Shape)=
+        backGroundStyle.apply(backGround)
+        foreGroundStyle.apply(foreGround)
+}
 
 class ProgressBar(
     var x: Float,
@@ -23,26 +23,24 @@ class ProgressBar(
     height: Float,
     var minValue: Float = 0f,
     var maxValue: Float = 1f,
-    var currentValue: Float = 0.5f
+    var currentValue: Float = 0.5f,
+    var style:ProgressBarStyle = ProgressBarStyle()
 ) extends UIComponent(width,height){
     this.globalBounds = Rect(this.x, this.y, this.width, this.height)
     
-    var backgroundBar = RectangleShape(this.width,this.height)
-    bgStyle.apply(backgroundBar)
-
-    
-    var progressBar = RectangleShape(this.width,this.height)
-    fgStyle.apply(progressBar)
+    var backGround = RectangleShape(this.width,this.height)
+    var foreGround = RectangleShape(this.width,this.height)
+    this.style.apply(backGround,foreGround)
 
 
     def updateValue(newValue: Float) = {
         currentValue =  max(minValue,min(newValue,maxValue))
         var fillProportion: Float = (currentValue - this.minValue) / (this.maxValue - this.minValue)
-        progressBar.scale(fillProportion,1f)
+        foreGround.scale(fillProportion,1f)
     }
 
 
     override def draw(target: RenderTarget, states: RenderStates) =
-        this.backgroundBar.draw(target,states)
-        this.progressBar.draw(target,states)
+        this.backGround.draw(target,states)
+        this.foreGround.draw(target,states)
 }
