59033012855e5dbcc92e25b1da982911aabea214
Parabrele <g.dhimoila@gmail.com>
2023-04-06 22:15:27 +0200

    add mother ship click behavior

--- src/main/scala/Actor/Ships/CapitalShip.scala
+++ src/main/scala/Actor/Ships/CapitalShip.scala
@@ -6,6 +6,10 @@ import shipmodule.ShipModule
 import manager.TextureManager
 import gamestate.*
 import ShipModules.*
+import event.KeyboardState
+import sfml.window.Keyboard.Key
+import clickable.States
+import controller.Camera
 
 //this is the capital ship class. They are large ship able to hold a lot of cargo and house a lot of modules.
 //Their behavior is similar to the drone, except their action can only be IDLE or MOVE, the modules they are controlling will do the rest.
@@ -56,4 +60,38 @@ class CapitalShip(
             for j <- 0 until shipDimension.y do
                 if modules(i)(j).isDefined then
                     modules(i)(j).get.updateModule()
+
+    //click behavior of the capital ships. They have to be pressed by a simple clic, not a hold clic with ctrl.
+    if this.teamID == 0 then
+        this.updateLeftClick = () =>
+            if KeyboardState.is_Press(Key.KeyLControl) then
+                if this.state == States.HOVER then
+                    this.onUnhovered(())
+                    this.state = States.IDLE
+            
+            else
+                if this.clickBounds.contains(KeyboardState.mouseView) then
+                    this.state = States.PRESSED
+                    this.onPressed(())
+                else
+                    if this.state == States.HOVER then
+                        this.onUnhovered(())
+                        this.state = States.IDLE
+            
+            if this.clickBounds.contains(KeyboardState.mouseView) && KeyboardState.is_Press(Key.KeyLAlt) then
+                Camera.updateBind(this)
+        
+        this.updateRightPress = () => ()
+
+        //this is necessary to ensure that selectioning targets do not release the actors selected by the player.
+        val general_right_hold = this.updateRightHold
+
+        this.updateRightHold = () =>
+            if this.state != States.PRESSED then
+                general_right_hold()
+        
+        this.updateRightClick = () =>
+            if this.state == States.HOVER then
+                this.state = States.IDLE
+                this.onUnhovered(())
 }
